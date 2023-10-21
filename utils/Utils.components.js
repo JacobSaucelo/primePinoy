7e71c193176852c2ddd9e3dbca77655fa1f4addf
csthesisgroup <jacobsaucelo32@gmail.com>
2023-10-21 15:20:34 +0800

    temporary images

--- utils/Utils.components.js
+++ utils/Utils.components.js
@@ -1,17 +1,67 @@
 const loadModule = (moduleId, filePath) => {
-  fetch(filePath)
-    .then((response) => response.text())
-    .then((html) => {
-      document.getElementById(moduleId).innerHTML = html;
-    })
-    .catch((err) => {
-      console.error("failed to load module", err);
-    });
+  return new Promise((resolve, reject) => {
+    fetch(filePath)
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById(moduleId).innerHTML = html;
+        resolve();
+      })
+      .catch((err) => {
+        console.error("failed to load module", err);
+        reject(err);
+      });
+  });
 };
 
-loadModule("componentHeroSection", "modules/Modules.HeroSection.html");
-loadModule("componentDoubleSection1", "modules/Modules.DoubleSection1.html");
-loadModule(
-  "componentFollowingSection",
-  "/modules/Modules.FollowingSection.html"
-);
+Promise.all([
+  loadModule("componentHeroSection", "modules/Modules.HeroSection.html"),
+  loadModule("componentDoubleSection1", "modules/Modules.DoubleSection1.html"),
+  loadModule(
+    "componentFollowingSection",
+    "/modules/Modules.FollowingSection.html"
+  ),
+  loadModule(
+    "componentCarouselSection1",
+    "/modules/Modules.CarouselSection1.html"
+  ),
+])
+  .then(() => {
+    $(document).ready(function () {
+      $(".carousel").carousel();
+    });
+  })
+  .catch((err) => {
+    console.log("failed module import", err);
+    $(document).ready(function () {
+      console.log("i should run");
+      $(".carousel").carousel();
+    });
+  });
+
+// const loadModule = (moduleId, filePath) => {
+//   fetch(filePath)
+//     .then((response) => response.text())
+//     .then((html) => {
+//       document.getElementById(moduleId).innerHTML = html;
+//     })
+//     .catch((err) => {
+//       console.error("failed to load module", err);
+//     });
+// };
+
+// Promise.all([
+//   loadModule("componentHeroSection", "modules/Modules.HeroSection.html"),
+//   loadModule("componentDoubleSection1", "modules/Modules.DoubleSection1.html"),
+//   loadModule(
+//     "componentFollowingSection",
+//     "/modules/Modules.FollowingSection.html"
+//   ),
+//   loadModule(
+//     "componentCarouselSection1",
+//     "/modules/Modules.CarouselSection1.html"
+//   ),
+// ]).then(() => {
+//   $(document).ready(function () {
+//     $(".carousel").carousel();
+//   });
+// });
