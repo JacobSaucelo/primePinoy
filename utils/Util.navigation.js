d8255681da3bd84047b4cf4bed29ee077454f997
csthesisgroup <jacobsaucelo32@gmail.com>
2023-10-26 07:36:49 +0800

    removed modules

--- utils/Util.navigation.js
+++ utils/Util.navigation.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
   $(".sidenav").sidenav({
     edge: "right",
   });
+  $(".carousel").carousel();
+  $(".parallax").parallax();
+  $(".carousel.carousel-slider2").carousel({
+    indicators: true,
+  });
+  $(".collapsible").collapsible();
+  $(".scrollspy").scrollSpy({ throttle: 800 });
 });
